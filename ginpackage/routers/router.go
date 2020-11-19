e737371f82e19a0a4b0edb81216cf05fd69745b3
wangliutong <wangliutong@mudu.tv>
2020-11-19 16:48:30 +0800

    蒸饺标签模块路由

--- ginpackage/routers/router.go
+++ ginpackage/routers/router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"GoGin/ginpackage/pkg/setting"
+	v1 "GoGin/ginpackage/routers/api/v1"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,10 +12,16 @@ func InitRouter() *gin.Engine {
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 	gin.SetMode(setting.RunMode)
-	r.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "test",
-		})
-	})
+	apiv1 := r.Group("/api/v1")
+	{
+		// 获取标签列表
+		apiv1.GET("/tags", v1.GetTags)
+		// 新建标签
+		apiv1.POST("/tags", v1.AddTags)
+		// 更新指定标签
+		apiv1.PUT("/tags", v1.EditTag)
+		// 删除指定标签
+		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+	}
 	return r
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module GoGin
 go 1.15
 
 require (
-	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/astaxie/beego v1.12.3 // indirect
+	github.com/gin-gonic/gin v1.6.3
 	github.com/go-ini/ini v1.62.0
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/unknwon/com v1.0.1 // indirect
